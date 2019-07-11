1fa4c32ed2831059564d8e442c41af53a39c68c6
Andrea Marrano <andrea.marrano@drivek.com>
2019-07-11 16:53:25 +0200

    added files

--- hbelv.php
+++ hbelv.php
@@ -8,7 +8,7 @@ Author: marrano92
 License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Domain Path: /languages
-Text Domain: dkwp
+Text Domain: hbelv
 */
 
 /********************************
@@ -17,3 +17,21 @@ Text Domain: dkwp
 
 defined( 'ABSPATH' ) || die( 'Error 403: Access Denied/Forbidden!' );
 defined( 'HOUR_IN_SECONDS' ) || define( 'HOUR_IN_SECONDS', 3600 );
+
+/**
+ * Autoloader init
+ */
+if ( file_exists( DKWP_PLUGIN_DIR . 'vendor/autoload.php' ) ) {
+	require_once DKWP_PLUGIN_DIR . 'vendor/autoload.php';
+}
+
+/**
+ * Plugins loaded action
+ *
+ * @return void
+ */
+function hbelv_plugins_loaded() {
+	load_plugin_textdomain( 'hbelv', false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
+}
+
+add_action( 'plugins_loaded', 'dkwp_plugins_loaded' );
