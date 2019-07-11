7039bb7f167b0f4bbfddc0fd485c4873ad2bb59f
Andrea Marrano <andrea.marrano@drivek.com>
2019-07-11 14:18:10 +0200

    creating plugin

---
+++ hbelv.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Hbelv
+Plugin URI: https://hotelilbelvedere.com
+Description: Engine for Hotel il Belvedere WordPress-powered
+Version: 0.1
+Author: marrano92
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
+Text Domain: dkwp
+*/
+
+/********************************
+ * Globals definitions *
+ *******************************/
+
+defined( 'ABSPATH' ) || die( 'Error 403: Access Denied/Forbidden!' );
+defined( 'HOUR_IN_SECONDS' ) || define( 'HOUR_IN_SECONDS', 3600 );
